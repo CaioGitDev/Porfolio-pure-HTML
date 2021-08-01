f400fd8dbf0b1020a775929695099bb0a339f27f
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2021-08-01 02:13:06 +0100

    feat: finisched skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
+//async operations
 document.addEventListener('DOMContentLoaded', async function () {
-
   const {
     getElementById,
     getAllElementsByClass,
@@ -34,13 +34,35 @@ document.addEventListener('DOMContentLoaded', async function () {
     elementRemoveClassList(navMenu, 'show-menu')
   }
   navLinkList.forEach(x => x.addEventListener('click', linkAction))
-
-
-  // ############ MENU SHOW / HIDE ###########################
-
-  // ############ MENU SHOW / HIDE ###########################
-
   // ############ MENU SHOW / HIDE ###########################
 
 
 });
+
+
+
+// ############ ADD SKILLS PERCENTAGE WIDTH #########################
+const elements = document.querySelectorAll('.skills__data');
+elements.forEach(element => {
+  var percentage = element.children[0].children[1].innerHTML;
+  element.children[1].children[0].style.width = percentage;
+});
+
+// ############ ACCORDION SKILLS ###########################
+const skillsContent = document.getElementsByClassName('skills__content'),
+skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+console.log(this)
+let itemClass = this.parentNode.className
+for (let i = 0; i < skillsContent.length; i++) {
+  skillsContent[i].className = 'skills__content skills__close';
+
+}
+if (itemClass === 'skills__content skills__close') {
+  this.parentNode.className = 'skills__content skills__open'
+}
+}
+skillsHeader.forEach((el) => {
+el.addEventListener('click', toggleSkills)
+})
