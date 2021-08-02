01b76a2d814f6d33149f06a0a463350f166852f4
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2021-08-02 22:29:18 +0100

    feat: create seed file

---
+++ assets/js/seeds.js
@@ -0,0 +1,50 @@
+const seedQuaficationsList = [
+  {
+    title: "Profissional Técnico de Multimédia",
+    subtitle: "Portugal - Salvaterra de Magos",
+    calendar: "2012 - 2014",
+    type: "education",
+  },
+  {
+    title: "Diploma Técnico grau 5 -Técnico Profissional de Sistemas de Informação",
+    subtitle: "Portugal - Santarem",
+    calendar: "2015 - 2017",
+    type: "education",
+  },
+  {
+    title: "GoStack - Fullstack with Node, React, SQLite",
+    subtitle: "Rocketseat",
+    calendar: "2020 - 2020",
+    type: "education",
+  },
+  {
+    title: "Developing ASP.NET Core MVC Web Applications",
+    subtitle: "Portugal - Lisbon",
+    calendar: "2020 - 2020",
+    type: "education",
+  },
+  {
+    title: "Web Design",
+    subtitle: "MicroAguas",
+    calendar: "2014 - 2015",
+    type: "work",
+  },
+  {
+    title: "FreeLancer - branding",
+    subtitle: "Portugal",
+    calendar: "2017 - 2018",
+    type: "work",
+  },
+  {
+    title: "Web Developer - Estágio",
+    subtitle: "IPO Lisboa",
+    calendar: "2018 - 2019",
+    type: "work",
+  },
+  {
+    title: "Fullstack Web Developer",
+    subtitle: "IPO Lisboa",
+    calendar: "2019 - 2021",
+    type: "work",
+  },
+]
